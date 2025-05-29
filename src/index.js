915c4fca023a668ff5d6d2ee0244650ba4736a0f
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-05-29 16:32:15 -0400

    Condense search function to loop move solution condensing to work with loop

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ knightMoves = function (start, end) {
       return console.log("Enter valid end point.");
     }
   }
-  console.log(start, end);
   let legalmoves = [
     [2, 1],
     [1, 2],
@@ -30,17 +29,7 @@ knightMoves = function (start, end) {
   moves(current, legalmoves, path, end, solution);
 
   for (let x = 0; x < solution.length - 1; x++) {
-    if (solution[x][0].length < solution[x + 1][0].length) {
-      solution.splice(x + 1, 1);
-      x--;
-    } else if (solution[x][0].length > solution[x + 1][0].length) {
-      solution.splice(x, 1);
-      x--;
-    }
-  }
-  for (let x = 0; x < solution.length - 1; x++) {
-    if (solution[x][0].length > solution[solution.length - 1][0].length) {
-      console.log("test");
+    if (solution[solution.length - 1].length < solution[x].length) {
       solution.splice(x, 1);
       x--;
     }
@@ -48,26 +37,26 @@ knightMoves = function (start, end) {
 
   if (solution.length > 1) {
     console.log(
-      `You made it in ${solution[0][0].length - 1} moves! Here are all ${
+      `You made it in ${solution[0].length - 1} moves! Here are all ${
         solution.length
       } of your potential paths:`
     );
-  } else if (solution[0][0].length === 2) {
+  } else if (solution[0].length === 2) {
     console.log(
       `You made it in ${
-        solution[0][0].length - 1
+        solution[0].length - 1
       } move! Here is the quickest way although you should be able to find this path without help:`
     );
   } else {
     console.log(
       `You made it in ${
-        solution[0][0].length - 1
+        solution[0].length - 1
       } moves! Here is your best solution out of all your potential path:`
     );
   }
 
   for (let x = 0; x < solution.length; x++) {
-    console.log(JSON.stringify(solution[x][0]));
+    console.log(JSON.stringify(solution[x]));
   }
 };
 
@@ -76,17 +65,19 @@ moves = function (current, legalmoves, path, end, solution) {
     path.push(current);
     return;
   }
-  if (path.length > 7) {
-    path.push(current);
+
+  path.push(current);
+
+  if (path.length >= 7) {
     return;
   }
+
   for (let x = 0; x < solution.length; x++) {
-    if (path.length > solution[x][0].length) {
-      path.push(current);
+    if (path.length >= solution[x].length) {
       return;
     }
   }
-  path.push(current);
+
   for (let y = 0; y < legalmoves.length; y++) {
     if (
       current[0] + legalmoves[y][0] === end[0] &&
@@ -95,154 +86,36 @@ moves = function (current, legalmoves, path, end, solution) {
       current = [current[0] + legalmoves[y][0], current[1] + legalmoves[y][1]];
       path.push(current);
       let tmp = path.slice();
-      solution.push([tmp]);
+      solution.push(tmp);
       path.pop();
       return solution;
     }
   }
+
   if (checkpath(path, end)) {
     return;
   }
-  if (
-    -1 < current[0] + legalmoves[0][0] &&
-    -1 < current[1] + legalmoves[0][1] &&
-    current[0] + legalmoves[0][0] < 8 &&
-    current[1] + legalmoves[0][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[0][0], current[1] + legalmoves[0][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[1][0] &&
-    -1 < current[1] + legalmoves[1][1] &&
-    current[0] + legalmoves[1][0] < 8 &&
-    current[1] + legalmoves[1][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[1][0], current[1] + legalmoves[1][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[2][0] &&
-    -1 < current[1] + legalmoves[2][1] &&
-    current[0] + legalmoves[2][0] < 8 &&
-    current[1] + legalmoves[2][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[2][0], current[1] + legalmoves[2][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[3][0] &&
-    -1 < current[1] + legalmoves[3][1] &&
-    current[0] + legalmoves[3][0] < 8 &&
-    current[1] + legalmoves[3][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[3][0], current[1] + legalmoves[3][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[4][0] &&
-    -1 < current[1] + legalmoves[4][1] &&
-    current[0] + legalmoves[4][0] < 8 &&
-    current[1] + legalmoves[4][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[4][0], current[1] + legalmoves[4][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[5][0] &&
-    -1 < current[1] + legalmoves[5][1] &&
-    current[0] + legalmoves[5][0] < 8 &&
-    current[1] + legalmoves[5][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[5][0], current[1] + legalmoves[5][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[6][0] &&
-    -1 < current[1] + legalmoves[6][1] &&
-    current[0] + legalmoves[6][0] < 8 &&
-    current[1] + legalmoves[6][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[6][0], current[1] + legalmoves[6][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
-  }
-  if (
-    -1 < current[0] + legalmoves[7][0] &&
-    -1 < current[1] + legalmoves[7][1] &&
-    current[0] + legalmoves[7][0] < 8 &&
-    current[1] + legalmoves[7][1] < 8
-  ) {
-    moves(
-      [current[0] + legalmoves[7][0], current[1] + legalmoves[7][1]],
-      legalmoves,
-      path,
-      end,
-      solution
-    );
-    path.pop();
+
+  for (let x = 0; x < legalmoves.length; x++) {
+    if (
+      current[0] + legalmoves[x][0] < 8 &&
+      current[0] + legalmoves[x][0] > -1 &&
+      current[1] + legalmoves[x][1] < 8 &&
+      current[1] + legalmoves[x][1] > -1
+    ) {
+      {
+        moves(
+          [current[0] + legalmoves[x][0], current[1] + legalmoves[x][1]],
+          legalmoves,
+          path,
+          end,
+          solution
+        );
+        path.pop();
+      }
+    }
   }
   return solution;
-
-  // if (
-  //   current[0] + legalmoves[x][0] < 8 &&
-  //   current[0] + legalmoves[x][0] > -1 &&
-  //   current[1] + legalmoves[x][1] < 8 &&
-  //   current[1] + legalmoves[x][1] > -1
-  // ) {
-  //   if (
-  //     !checkpath(path, [
-  //       current[0] + legalmoves[x][0],
-  //       current[1] + legalmoves[x][1],
-  //     ])
-  //   ) {
-  //     current = [current[0] + legalmoves[x][0], current[1] + legalmoves[x][1]];
-  //     path.push(current);
-  //     moves(current, legalmoves, path, end, solution);
-  //     return;
-  //   }
-  // }
 };
 
 checkpath = function (path, array) {
@@ -254,4 +127,4 @@ checkpath = function (path, array) {
   return false;
 };
 
-knightMoves([0, 0], [1, 2]);
+knightMoves([0, 0], [2, 2]);
